7f59d90b4348e6ede0f42aa1d72f4153aedd9ffa
Gamineepatle <147207123+Gamineepatle@users.noreply.github.com>
2023-10-10 00:57:32 +0530

    Update BaseClass.java

--- src/main/java/smartTech/basePage/BaseClass.java
+++ src/main/java/smartTech/basePage/BaseClass.java
@@ -28,6 +28,7 @@ public class BaseClass {
         driver.get(prop.getProperty("url"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //
 
 //    return driver;
     }
